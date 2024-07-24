9f0768ae2920370c367f7c8a2f9760a004dfadf9
Elton Chaves <chavesejf@gmail.com>
2024-07-24 15:08:39 -0300

    Add files via upload

--- pbee.py
+++ pbee.py
@@ -272,7 +272,7 @@ def get_interface_features(pdbfile,ions,xml,outdir,submit_dir):
         rosetta_scripts.default.linuxgccrelease \
         -s {pdbfile} \
         -parser:protocol {xml} \
-        -holes:dalphaball $ROSETTA3/source/external/DAlphaBall/DAlphaBall.gcc \
+        -holes:dalphaball $ROSETTA3/source/external/DAlpahBall/DAlpahBall.gcc \
         -ex1 -ex2 -ex2aro \
         -auto_setup_metals \
         -beta_nov16 \
@@ -290,7 +290,7 @@ def get_interface_features(pdbfile,ions,xml,outdir,submit_dir):
         rosetta_scripts.default.linuxgccrelease \
         -s {pdbfile} \
         -parser:protocol {xml} \
-        -holes:dalphaball $ROSETTA3/source/external/DalphaBall/DAlphaBall.gcc \
+        -holes:dalphaball $ROSETTA3/source/external/DAlpahBall/DAlpahBall.gcc \
         -ex1 -ex2 -ex2aro \
         -beta_nov16 \
         -use_input_sc \
